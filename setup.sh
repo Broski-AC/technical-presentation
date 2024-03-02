e910583e42d625798c94e1f6336a4f243f42f173
Allison Broski <acbroski@oakland.edu>
2024-03-01 19:53:51 -0600

    feat: get variable set up

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Set an environment variable in /root/.bash_profile
+## Create variable for environemnt value
+username_command="export GITHUB_USERNAME=Broski-AC"
+
+## Check if file exists; if not, create and fill with info
+
 
 # Write code to a file called config.hcl, replacing variable <HOSTNAME> with vault-server.hashicorp.com
