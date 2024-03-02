2db6ad0b8015fde3cd787b6ced1e2f50978cc3a4
Allison Broski <acbroski@oakland.edu>
2024-03-01 20:16:42 -0600

    feat: basic script for writing env var to file

--- setup.sh
+++ setup.sh
@@ -4,7 +4,16 @@
 ## Create variable for environemnt value
 username_command="export GITHUB_USERNAME=Broski-AC"
 
-## Check if file exists; if not, create and fill with info
+## Check if file exists; if not, create
+if [[ ! -e ~/root/.bash_profile ]];
+then
+    mkdir -p ~/root
+    touch ~/root/.bash_profile
+    chmod +rw ~/root/.bash_profile
+fi
 
+## Add username_command value to file
+### This will overwrite existing info in the file; if we wanted to append, use >>
+echo "$username_command" > ~/root/.bash_profile
 
 # Write code to a file called config.hcl, replacing variable <HOSTNAME> with vault-server.hashicorp.com
