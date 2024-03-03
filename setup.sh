2beb68736f29c0e3ef732e7f305b95fbceec5774
Allison Broski <acbroski@oakland.edu>
2024-03-02 19:22:30 -0600

    style: uses best practices for bash

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -eo pipefail
 
 # Set an environment variable in /root/.bash_profile
-function setup_env {
+setup_env() {
+
     ## Create variable for environemnt value
     username_command="export GITHUB_USERNAME=Broski-AC"
 
@@ -15,34 +17,43 @@ function setup_env {
 
     ## Add username_command value to file
     ### This will overwrite existing info in the file; if we wanted to append, use >>
-    echo "$username_command" > ~/root/.bash_profile
+    echo "${username_command}" > ~/root/.bash_profile
     return
+
 }
 
 # Write code to a file called config.hcl, replacing variable <HOSTNAME> with vault-server.hashicorp.com
-function setup_config {
-    #Right now will make a duplicate copy of the file if it already exists
-    cat <<EOF > config.hcl
+setup_config() {
+
+cat <<EOF > config.hcl
 cluster_addr  = "https://<HOSTNAME>:8201"
 api_addr      = "https://<HOSTNAME>:8200"
 disable_mlock = true
 EOF
 
-# -i does in-place edit, -e appends all info to the command; /g does a global replace
-sed -i -e 's/<HOSTNAME>/vault-server.hashicorp.com/g' ./config.hcl
+# -i does in-place edit, '' is necessary for macOS; /g does a global replace
+sed -i '' 's/<HOSTNAME>/vault-server.hashicorp.com/g' ./config.hcl
+return 
 }
 
-function result_announcement {
+result_announcement() {
     if [[ $1 -ne 0 ]]; 
     then
         echo "$2 failure with code $1"
     else
         echo "$2 success"
     fi
+    return
 }
 
-env_output="$(setup_env)"
-result_announcement "$env_output" "setup_env()"
+main() {
+    env_output="$(setup_env)"
+    result_announcement "${env_output}" "setup_env()"
 
-env_out="$(setup_config)"
-result_announcement "env_output" "setup_config()"
+    config_output="$(setup_config)"
+    result_announcement "${config_output}" "setup_config()"
+}
+
+main
+
+
